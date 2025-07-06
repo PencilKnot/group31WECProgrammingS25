082994eea0770eecf749fc559a287b07242ffb94
PencilKnot <wingchang.ca@gmail.com>
2025-07-06 10:39:40 -0400

    Added activity class

---
+++ activity.py
@@ -0,0 +1,11 @@
+class Activity:
+    def __init__(self, category, name, type, location, start, end, day):
+        self.name = name
+        self.category = category
+        self.type = type
+        self.location = location
+        self.start = start
+        self.end = end
+        self.day = day
+    
+    
