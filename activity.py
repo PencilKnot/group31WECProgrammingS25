af358b285cd1d20a7b2a99a87401fcfeead73d67
happyface3650 <syuen2010113@gmail.com>
2025-07-06 14:04:01 -0400

    input done:)

--- activity.py
+++ activity.py
@@ -1,5 +1,5 @@
 class Activity:
-    def __init__(self, category, name, school_ac, location, oncampus, start, end, day):
+    def __init__(self, category="", name="", school_ac=0, location="", oncampus=False, start=0, end=0, day=""):
         self.name = name
         self.category = category
         self.school_ac = school_ac
--- textedit.py
+++ textedit.py
@@ -6,8 +6,8 @@ from activity import Activity
 # if the file is in a different directory provide the relative path
 
 file1 = open("sampleinput.txt","r")
-file_content = file1.read() # stores the entire file content of the file as a string into the file_content variable
-file_content = file_content.replace('(', '').replace(')', '') #remove brackets
+#file_content = file1.read() # stores the entire file content of the file as a string into the file_content variable
+ #remove brackets
 file_lines = file1.readlines() # stores each line of the file as an element of a list
 
 def split_time(times):
@@ -15,7 +15,7 @@ def split_time(times):
     returns time as an integer value of the nth minute of the day
     '''
     time = times.split(':') #splits time into where time[0] = hours and time[1] = minutes
-    return (time[0]*60 + time[1])
+    return (int(time[0])*60 + int(time[1]))
 
 def assign_attributes(line, obj):
     '''
@@ -23,10 +23,14 @@ def assign_attributes(line, obj):
     '''
     obj.school_ac = 0
     obj.location = line[1]
-    if(line[2]=="false"):
+    if(line[2]==" false"):
         obj.oncampus = False
     else:
         obj.oncampus = True
+    times = line[3].split('-') #split start and end times
+    obj.start = split_time(times[0]) #start time as an integer
+    obj.end = split_time(times[1]) #end time as an integer
+    
 
 
 
@@ -36,43 +40,51 @@ num_EC = None
 num_social = None
 
 for i in range (len(file_lines)):
-    line = file_lines[i].split(',') #split things by comma 
-
+    file_line = file_lines[i].replace('(', '').replace(')', '').replace('\n','') # deletes brackets
+    line = file_line.split(',') #split things by comma
+    print (line)
     if "SCHOOL" in line:
-        num_school = line[1] #get number of school events
+        num_school = int(line[1]) #get number of school events
         school_start = i
     elif "ECs" in line:
-        num_EC = line[1] #get number of EC
+        num_EC = int(line[1]) #get number of EC
         EC_start = i
     elif "SOCIAL" in line:
-        num_social = line[1] #get number of socials
+        num_social = int(line[1]) #get number of socials
         social_start = i
     else:
-        event_obj = Activity()
-        event_obj.name = line[0]
+        if ('' not in line):
+            event_obj = Activity()
+            event_obj.name = line[0]
 
-        if(num_school is not None):
-            if(i>school_start and i<=school_start+num_school):
-                event_obj.category = "School"
-                event_obj.school_ac = line[1]
-                event_obj.location = line[2]
-                event_obj.oncampus = True
-                event_obj.day = line[4]
-        
-        if(num_EC is not None):
-            if(i> EC_start and i<=EC_start+num_EC):
-                event_obj.category = "EC"
-                assign_attributes(line, event_obj)
-        
-        if(num_social is not None):
-            if(i> social_start and i<=social_start+num_social):
-                event_obj.category = "Social"
-                assign_attributes(line, event_obj)
-                
-        times = line[3].split('-') #split start and end times
-        event_obj.start = split_time(times[0]) #start time as an integer
-        event_obj.end = split_time(times[1]) #end time as an integer
-        Activity_list.append(event_obj)
+            if(num_school is not None):
+                if(i>school_start and i<=school_start+num_school):
+                    event_obj.category = "School"
+                    event_obj.school_ac = line[1]
+                    event_obj.location = line[2]
+                    event_obj.oncampus = True
+                    day_and_time = line[3].split(' ')
+                    times = day_and_time[1].split('-')
+                    event_obj.start = split_time(times[0])
+                    event_obj.end = split_time(times[1])
+                    event_obj.day = day_and_time[2]
+
+
+
+                    
+            
+            if(num_EC is not None):
+                if(i> EC_start and i<=EC_start+num_EC):
+                    event_obj.category = "EC"
+                    assign_attributes(line, event_obj)
+            
+            if(num_social is not None):
+                if(i> social_start and i<=social_start+num_social):
+                    event_obj.category = "Social"
+                    assign_attributes(line, event_obj)
+            Activity_list.append(event_obj)
+
+            
         
      
 for x in range (len(Activity_list)):
