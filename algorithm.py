c9a78616b2a6d664b5697eb816736a8b2da5a5c3
PencilKnot <wingchang.ca@gmail.com>
2025-07-06 12:51:45 -0400

    Added basic functions for algorithm

--- algorithm.py
+++ algorithm.py
@@ -13,12 +13,32 @@ def enough_travel_time(a1, a2):
 def during_sleep(a):
     return True if (a.start < 1380 and a.end < 1380 and a.start > 420 and a.end > 420) else False
 
-# put all school activities with dates into schedule first
-for activity in activity_list:
-    if activity.category == 'School' and activity.day:
-        schedule[activity.day].append(activity)
+def get_priority(a):
+    match a.type:
+        case 'Lecture': return 2
+        case 'Lab': return 1.5
+        case 'Tutorial': return 0.75
 
-# sort each day by start time (ASSUME NO CONFLICTS???)
-for day in schedule:
-    schedule[day].sort(key=lambda activity: activity.start)
-    
+def optimize_schedule(activity_list):
+    # put all school activities with dates into schedule first
+    for day in days:
+        current_day = []
+        i = 1
+        for a in activity_list:
+            if(a.category == 'School' and a.day == day and not(during_sleep(a))):
+                current_day.append(a);
+        current_day.sort(key=lambda a: a.start)
+
+        while i < len(current_day):
+            prev = current_day[i-1]
+            curr = current_day[i]
+
+            if has_conflict(prev, curr) or not enough_travel_time(prev, curr):
+                if(get_priority(prev) > get_priority(curr)):
+                    current_day.pop(i)
+                else:
+                    current_day.pop(i-1)
+                    i = max(1, i-1)
+            else: i += 1
+
+        schedule[day] = current_day;
